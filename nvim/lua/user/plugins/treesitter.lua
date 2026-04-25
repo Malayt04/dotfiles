362c720c668b3c8b035d7090214e1d6020e75172
Malay <malayt04@gmail.com>
2026-04-25 11:55:11 +0530

    feat: add typescript, tsx, go, and web development language support to treesitter and LSP configuration

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -5,7 +5,7 @@ vim.cmd('packadd nvim-treesitter')
 local status, configs = pcall(require, 'nvim-treesitter.configs')
 if status then
 	configs.setup({
-		ensure_installed = { "php", "html", "css", "javascript", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "blade" },
+		ensure_installed = { "php", "html", "css", "javascript", "typescript", "tsx", "go", "gomod", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "blade" },
 		highlight = {
 			enable = true,
 			additional_vim_regex_highlighting = false,
